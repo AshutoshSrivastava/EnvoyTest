cf77197687a510394c4edf3cb3625d86ab18c21d
AshutoshSrivastava <erashu@live.in>
2020-10-05 01:54:47 +0530

    initial

---
+++ CoffeeApi/Controllers/CoffeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CoffeeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeController : ControllerBase
+    {
+        private static readonly string[] Coffees = new[]
+        {
+            "Affogato","Americano","Caffe Latte","CafèAuLait","CAPPUCCINO","COLD BREW COFFEE","DOUBLE ESPRESSO (DOPPIO)","ESPRESSO","ESPRESSO CON PANNA","ESPRESSO MACCHIATO","FLAT WHITE","FRAPPÉ","IRISH COFFEE"
+        };
+
+        private readonly ILogger<CoffeeController> _logger;
+
+        public CoffeeController(ILogger<CoffeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var rng = new Random();
+            return Ok(Coffees[rng.Next(Coffees.Length)]);
+
+            // return Enumerable.Range(1, 5).Select(index => new Coffee
+            // {
+            //     Date = DateTime.Now.AddDays(index),
+            //     TemperatureC = rng.Next(-20, 55),
+            //     Summary = Summaries[rng.Next(Summaries.Length)]
+            // })
+            // .ToArray();
+        }
+    }
+}
---
+++ TeaApi/Controllers/TeaController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TeaApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeaController : ControllerBase
+    {
+        private static readonly string[] Teas = new[]
+        {
+"Cardamom tea",
+"Green tea.",
+"Oolong Tea.",
+"Matcha.",
+"White Tea.",
+"Pu-erh.",
+"Yellow tea.",
+"Mulberry black tea.",
+"Chai.",
+"Barley tea",
+"Camomile tea",
+"Chrysanthemum tea",
+"Hibiscus tea",
+"Honey tea",
+"Mint ginger lemon tea",
+"Peppermint",
+"Rooibos / Lemon Rooibos",
+"Rosehip tea",
+"Spearmint tea",
+"Tulsi or Holy Basil tea",
+"Yerba Mate"
+        };
+
+        private readonly ILogger<TeaController> _logger;
+
+        public TeaController(ILogger<TeaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var rng = new Random();
+            return Ok(Teas[rng.Next(Teas.Length)]);
+            
+            // return Enumerable.Range(1, 5).Select(index => new Tea
+            // {
+            //     Date = DateTime.Now.AddDays(index),
+            //     TemperatureC = rng.Next(-20, 55),
+            //     Summary = Summaries[rng.Next(Summaries.Length)]
+            // })
+            // .ToArray();
+        }
+    }
+}
